fbe57df4e44db6248c759c09f6c74442a66716ab
Vikash Kumar <vikashkumarnokha@gmail.com>
2023-08-16 14:42:34 +0530

    cart, loading and pagination componenet added for home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,53 @@
 import React from 'react'
+import "../css/Home.css"
 import PrimarySearchAppBar from '../components/Navbar'
 import Footer from '../components/Footer'
+import RecipeReviewCard from '../components/card'
+import PaginationRounded from '../components/pagination'
+import CircularIndeterminate from '../components/Loading'
 
 
 export default function Home() {
   return (
     <div>
       <PrimarySearchAppBar/>
-      Home
-       <Footer/>
 
-        
+        {/* <CircularIndeterminate/> */}
+       <div className='cart-container' >
+        <div className='cart-box' >
+         <RecipeReviewCard/>
+        </div>
+        <div className='cart-box' >
+         <RecipeReviewCard/>
+        </div>
+        <div className='cart-box' >
+         <RecipeReviewCard/>
+        </div>
+        <div className='cart-box' >
+         <RecipeReviewCard/>
+        </div>
+        <div className='cart-box' >
+         <RecipeReviewCard/>
+        </div>
+        <div className='cart-box' >
+         <RecipeReviewCard/>
+        </div>
+        <div className='cart-box' >
+         <RecipeReviewCard/>
+        </div>
+        <div className='cart-box' >
+         <RecipeReviewCard/>
+        </div>
+        <div className='cart-box' >
+         <RecipeReviewCard/>
+        </div>
+       </div>
+      
+      <div className='pagination' >
+       <PaginationRounded/>
+      </div>
+      
+       <Footer/>
 
     </div>
   )
